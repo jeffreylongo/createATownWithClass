849bcc4d1728a24fab28aae9c9e4169e948c28d0
Jeff Longo <jeffreylongo@yahoo.com>
2017-03-27 14:02:14 -0400

    made classes, filled bank and house out

---
+++ createATownWithClass/House.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace createATownWithClass
+{
+    class House
+    {
+        //Name as a string
+        public string Name { get; set; }
+        //BuildingType as string, that is set in the constructor
+        public string BuildingType { get; set; }
+        //NumberOfRoom as an int
+        public int NumberOfRoom { get; set; }
+        //ConstructionMaterial as a string
+        public string ConstructionMaterial { get; set; }
+        //Height as double
+        public double Height { get; set; }
+        //Width as double
+        public double Width { get; set; }
+        //Length as Double
+        public double Length { get; set; }
+        //Square Footage as a Read Only, calculated field
+        public int SquareFootage { get; }
+        //Floors as integer
+        public int Floors { get; set; }
+        //BedRooms as integer
+        public int BedRooms { get; set; }
+        //BathRooms as integer
+        public int BathRooms { get; set; }
+        //Volume as a Read Only, calculated field
+        public int Volume { get; }
+        //FullAddress as a Read only, string value that is made of the Address1, Address2, city, state, zip Properties
+        public string FullAddress { get; set; }
+        //Current Occupants as read only integer
+        public int CurrentOccupants { get; }
+        //IncreateOccupants as a public method that increments the CurrentOccupants by 1
+        //DecreaseOccupants as a public method that decrements the CurrentOccupants by 1
+    }
+}
---
+++ createATownWithClass/School.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace createATownWithClass
+{
+    class School
+    {
+        //Name as a string
+        //BuildingType as string, that is set in the constructor
+        //NumberOfRoom as an int
+        //ConstructionMaterial as a string
+        //Height as double
+        //Width as double
+        //Length as Double
+        //Square Footage as a Read Only, calculated field
+        //Floors as integer
+        //Classrooms as integer
+        //BathRooms as integer
+        //Volume as a Read Only, calculated field
+        //FullAddress as a Read only, string value that is made of the Address1, Address2, city, state, zip Properties
+        //Current Occupants as read only integer
+        //IncreateOccupants as a public method that increments the CurrentOccupants by 1
+        //DecreaseOccupants as a public method that decrements the CurrentOccupants by 1
+        //NumberOfStudents as int
+        //InSession as Boolean
+        //NumberOfTeachers as a Integer
+        //TeacherStudentRatio as a method that returns a string in the format ${teacher}:{student}
+    }
+}
